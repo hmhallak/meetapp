6a45e8ad0ebd6ac4534ee679de64e3872cfc64c6
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-08-31 14:58:01 -0400

    adds listing for the meetups organized by the user

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
+import OrganizerController from './app/controllers/OrganizerController';
 import SubscriptionController from './app/controllers/SubscriptionController';
 import NotificationController from './app/controllers/NotificationController';
 
@@ -24,6 +25,8 @@ routes.get('/meetups', MeetupController.index);
 routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
+
+routes.get('/organizer', OrganizerController.index);
 
 routes.get('/subscriptions', SubscriptionController.index);
 routes.post('/meetups/:meetupId/subscriptions', SubscriptionController.store);
